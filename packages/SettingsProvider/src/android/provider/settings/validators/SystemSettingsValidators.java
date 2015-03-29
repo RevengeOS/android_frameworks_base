6338ff5aab548ef5639e6e79e3bf9f2cc86ffe1c
maxwen <max.weninger@gmail.com>
2015-03-29 14:00:00 +0100

    base: Allow disabling screenshot shutter sound [1/2]
    Includes also:
    
    commit b794cab075a38caccbb3c411b8b697426df27653
    Co-Authored-By: xyyx <xyyx@mail.ru>
    Date:   Thu Jan 12 21:59:42 2017 +0800
    
        ScreenshotSound: Fix playing screenshot's sound when camera's sounds off.
        This bug actually because Screenshot uses Camera Shutter Sound
    
    @idoybh: Adapt to A11
    
    Change-Id: If6f766e4b581e7c35d36e2e777ce2c37a3843ad4

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -221,5 +221,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.KEY_APP_SWITCH_ACTION, new InclusiveIntegerRangeValidator(0, 9));
         VALIDATORS.put(System.KEY_APP_SWITCH_LONG_PRESS_ACTION, new InclusiveIntegerRangeValidator(0, 9));
         VALIDATORS.put(System.LOCKSCREEN_MEDIA_ART, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.SCREENSHOT_SOUND, BOOLEAN_VALIDATOR);
     }
 }
