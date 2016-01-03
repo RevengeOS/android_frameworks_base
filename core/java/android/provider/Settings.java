95e47249f822874bba79911d0b3fd52d76b39b5d
htchoi1995 <htchoi1995@gmail.com>
2016-01-03 20:26:36 +0100

    base: Disable/Enable screenshot sound
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76
    Signed-off-by: chandra prakash <scp.thedevil@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4841,6 +4841,12 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
