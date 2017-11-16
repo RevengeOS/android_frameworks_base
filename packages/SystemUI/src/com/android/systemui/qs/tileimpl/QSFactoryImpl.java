c2920b75f41e8c81cb12711380f193615b83a1cf
Altaf-Mahdi <altaf.mahdi@gmail.com>
2017-11-15 22:36:51 -0500

    Add Screenshot QS Tile
    Change-Id: I9961ed353a9539a582a5fa3fdc4ffbaa10bab031

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -113,6 +114,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ExpandedDesktopTile(mHost);
             case "gaming":
                 return new GamingModeTile(mHost);
+	    case "screenshot":
+                return new ScreenshotTile(mHost);
         }
 
         // Intent tiles.
