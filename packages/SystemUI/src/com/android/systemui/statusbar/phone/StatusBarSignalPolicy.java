69fbbd4ca70928b6d3090edec0aaeb341ba72738
Paul Keith <javelinanddart@gmail.com>
2018-05-28 04:23:30 +0200

    StatusBarSignalPolicy: Hide signal icons for disabled SIMs
    @xyyx: updated to Pie

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -22,6 +22,7 @@ import android.graphics.Rect;
 import android.os.Handler;
 import android.os.Looper;
 import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
 import android.util.ArraySet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -43,9 +44,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.android.internal.telephony.util.TelephonyExtUtils;
+import com.android.internal.telephony.util.TelephonyExtUtils.ProvisioningChangedListener;
 
 public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallback,
-        SecurityController.SecurityControllerCallback, Tunable {
+        SecurityController.SecurityControllerCallback, Tunable, ProvisioningChangedListener {
     private static final String TAG = "StatusBarSignalPolicy";
 
     private final String mSlotAirplane;
@@ -55,6 +58,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
     private final String mSlotVpn;
 
     private final Context mContext;
+    private static Context ctx;
     private final StatusBarIconController mIconController;
     private final NetworkController mNetworkController;
     private final SecurityController mSecurityController;
@@ -76,6 +80,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
 
     public StatusBarSignalPolicy(Context context, StatusBarIconController iconController) {
         mContext = context;
+        ctx = context;
 
         mSlotAirplane = mContext.getString(com.android.internal.R.string.status_bar_airplane);
         mSlotMobile   = mContext.getString(com.android.internal.R.string.status_bar_mobile);
@@ -91,6 +96,24 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
 
         mNetworkController.addCallback(this);
         mSecurityController.addCallback(this);
+        TelephonyExtUtils.getInstance(context).addListener(this);
+    }
+
+    @Override
+    public void onProvisioningChanged(int slotId, boolean isProvisioned) {
+        int[] subId = SubscriptionManager.getSubId(slotId);
+        if (subId != null) {
+            MobileIconState state = getState(subId[0]);
+            if (state != null) {
+                state.mProvisioned = isProvisioned;
+                if (!isProvisioned) {
+                    state.visible = false;
+                }
+            }
+
+            mNetworkController.removeCallback(this);
+            mNetworkController.addCallback(this);
+        }
     }
 
     public void destroy() {
@@ -193,7 +216,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         // Visibility of the data type indicator changed
         boolean typeChanged = statusType != state.typeId && (statusType == 0 || state.typeId == 0);
 
-        state.visible = statusIcon.visible && !mBlockMobile;
+        state.visible = statusIcon.visible && !mBlockMobile && state.mProvisioned;
         state.strengthId = statusIcon.icon;
         state.typeId = statusType;
         state.contentDescription = statusIcon.contentDescription;
@@ -396,10 +419,15 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         public boolean needsLeadingPadding;
         public String typeContentDescription;
         public int volteId;
+        private boolean mProvisioned = true;
 
         private MobileIconState(int subId) {
             super();
             this.subId = subId;
+            TelephonyExtUtils extTelephony = TelephonyExtUtils.getInstance(ctx);
+            if (extTelephony.hasService()) {
+                mProvisioned = extTelephony.isSubProvisioned(subId);
+            }
         }
 
         @Override
