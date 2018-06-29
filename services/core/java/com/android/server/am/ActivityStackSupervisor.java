82299c36689d6dbeba797a666748e50cbd0506aa
qiushi.han <qiushi.han@spreadtrum.com>
2018-06-29 13:53:25 +0800

    Remove another unused calling for better performance
    getWindowContainerBounds will be called many times, trying to hold
    WMS lock, leading to bad performance, but no one will use this
    taskBounds.
    
    Test: make services
    Change-Id: I5d1cd162643fdc15dc1bc739abcf853ebd2dc91d

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -4516,7 +4516,6 @@ public class ActivityStackSupervisor extends ConfigurationContainer implements D
         }
         info.taskIds = taskIds;
         info.taskNames = taskNames;
-        info.taskBounds = taskBounds;
         info.taskUserIds = taskUserIds;
 
         final ActivityRecord top = stack.topRunningActivityLocked();
