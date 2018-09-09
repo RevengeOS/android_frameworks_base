f524c8643c925463c04941a05e4a47d01cea3b83
maxwen <max.weninger@gmail.com>
2018-09-09 22:11:07 +0200

    base: SystemUI: add qs quick tile column auto to follow columns
    always use same columns as the expanded panel but
    never less then 6
    
    Change-Id: I13f5f871cc1362d1fb8c60ba07aeff9675f1c32a

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -686,4 +686,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             }
         }
     }
+
+    public int getNumColumns() {
+        return mTileLayout.getNumColumns();
+    }
 }
--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -84,8 +84,9 @@ public class QuickQSPanel extends QSPanel {
         super.onDetachedFromWindow();
     }
 
-    public void setQSPanelAndHeader(QSPanel fullPanel, View header) {
+    public void setQSPanel(QSPanel fullPanel) {
         mFullPanel = fullPanel;
+        updateSettings();
     }
 
     @Override
@@ -167,11 +168,28 @@ public class QuickQSPanel extends QSPanel {
         super.setVisibility(visibility);
     }
 
+    public int getNumColumns() {
+        if (mFullPanel != null) {
+            return mFullPanel.getNumColumns();
+        }
+        return NUM_QUICK_TILES_DEFAULT;
+    }
+
     public void updateSettings() {
         int qsColumns = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.OMNI_QS_QUICKBAR_COLUMNS,
                 NUM_QUICK_TILES_DEFAULT, UserHandle.USER_CURRENT);
-        setMaxTiles(qsColumns);
+        if (qsColumns == -1) {
+            setMaxTiles(Math.max(NUM_QUICK_TILES_DEFAULT, getNumColumns()));
+        } else {
+            setMaxTiles(Math.max(NUM_QUICK_TILES_DEFAULT, qsColumns));
+        }
+    }
+
+    public void updateResources() {
+        if (mTileLayout != null) {
+            mTileLayout.updateResources();
+        }
     }
 
     private static class HeaderTileLayout extends LinearLayout implements QSTileLayout {
@@ -199,7 +217,7 @@ public class QuickQSPanel extends QSPanel {
         protected void onConfigurationChanged(Configuration newConfig) {
             super.onConfigurationChanged(newConfig);
 
-            setGravity(Gravity.CENTER);
+            /*setGravity(Gravity.CENTER);
             LayoutParams staticSpaceLayoutParams = generateSpaceLayoutParams(
                     mContext.getResources().getDimensionPixelSize(
                             R.dimen.qs_quick_tile_space_width));
@@ -212,7 +230,7 @@ public class QuickQSPanel extends QSPanel {
                 if (childView instanceof Space) {
                     childView.setLayoutParams(staticSpaceLayoutParams);
                 }
-            }
+            }*/
         }
 
         /**
@@ -291,7 +309,7 @@ public class QuickQSPanel extends QSPanel {
 
         @Override
         public boolean updateResources() {
-            // No resources here.
+            updateSettings();
             return false;
         }
 
@@ -315,8 +333,10 @@ public class QuickQSPanel extends QSPanel {
                         R.id.expand_indicator);
             }
         }
+
         @Override
         public void updateSettings() {
+            mPanel.updateSettings();
         }
 
         @Override
