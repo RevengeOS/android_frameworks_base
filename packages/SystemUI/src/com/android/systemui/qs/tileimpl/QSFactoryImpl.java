c14e1b43ee9256a995fe59d39964c0c2fcfeb449
Lucchetto <lucchetto.tie@live.com>
2018-09-18 21:15:09 +0200

    Revert "Reboot/Recovery QS Tile"
    This reverts commit 9eb4d3e91c58d6310ea390b7e17699360fd268e1.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,7 +39,6 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
-import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
@@ -107,8 +106,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundTile(mHost);
             case "caffeine":
                 return new CaffeineTile(mHost);
-            case "reboot":
-                return new RebootTile(mHost);
             case "heads_up":
                 return new HeadsUpTile(mHost);
             case "usb_tether":
