1ace8ec065297e9dfe6ba9d747f85d63dd4629c9
Lucchetto <lucchetto.tie@live.com>
2018-09-18 20:58:41 +0200

    Revert "Framework: Volume key cursor control (1/2)"
    This reverts commit 88594ce72f599ef8d0d6e13a13d73ae19a66aa36.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4296,15 +4296,6 @@ public final class Settings {
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
 
         /**
-          * Volume keys control cursor in text fields (default is 0)
-          * 0 - Disabled
-          * 1 - Volume up/down moves cursor left/right
-          * 2 - Volume up/down moves cursor right/left
-          * @hide
-          */
-        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
-
-        /**
          * @hide
          */
         public static final String TORCH_LONG_PRESS_POWER_GESTURE = "torch_long_press_power_gesture";
