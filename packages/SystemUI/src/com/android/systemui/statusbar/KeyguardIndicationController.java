f4dce6f2c138c5713cc3108eb029a70b6ef4cadf
Jens Lody <jens@jenslody.de>
2018-10-31 08:37:37 +0100

    Fix current formating on lockscreen for devices, that report directly in Ampere.
    Shamelessly extracted from:
      base: Lockscreen Charging info  (3/3)
      by: xyyx <xyyx@mail.ru>
    
    original commit-message:
    
     base: Lockscreen Charging info (3/3)
    
     based on: [2/2] frameworks: show charging current & voltage on lockscreen by
     yank555-lu [2/2]Base: show more battery info on lockscreen when charging by
     yank555-lu LockscreenCharging: Use double for mChargingVoltage and show as
     4.XV by xyyx Lockscreen charging: Formatting improvements by nathanchance
    
    AICPfy:
    - default set to false
    - Current formatting improvement for devices reporting directly in Amps
    
    Signed-off-by: Jens Lody <jens@jenslody.de>
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -436,7 +436,10 @@ public class KeyguardIndicationController {
         String batteryInfo = "";
 
         if (mChargingCurrent > 0) {
-            batteryInfo = batteryInfo + (mChargingCurrent / 1000) + "mA";
+           batteryInfo = batteryInfo + (mChargingCurrent < 5 ?
+                          (mChargingCurrent * 1000) : (mChargingCurrent < 4000 ?
+                          mChargingCurrent : (mChargingCurrent / 1000))) + "mA" ;
+
         }
         if (mChargingVoltage > 0) {
             batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
