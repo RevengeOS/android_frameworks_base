ab927b85985bc5e78dbb2e9046d4b7eb90dbe324
Lucchetto <lucchetto.tie@live.com>
2018-11-18 19:02:33 +0100

    Always block sensors for all packages
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4809,12 +4809,6 @@ public final class Settings {
          */
 
         /**
-         * Sensor block per-package
-         * @hide
-         */
-        public static final String SENSOR_BLOCK = "sensor_block";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
