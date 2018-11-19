1b19d1750c800709300a3f2139afe20b4271db8e
Lucchetto <lucchetto.tie@live.com>
2018-11-19 16:17:03 +0100

    Fix derp

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4394,12 +4394,6 @@ public final class Settings {
         private static final Validator BLUETOOTH_ACCEPT_ALL_FILES_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
-         * Sensor block per-package
-         * @hide
-         */
-        public static final String SENSOR_BLOCK = "sensor_block";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
