b34c9602b90c35a47d2104dedf2c6f0ff1c9c970
Lucchetto <lucchetto.tie@live.com>
2018-12-16 13:37:49 +0100

    Tint active volume bar with gradient
    Change-Id: I6528a1d06daf66c3b5f83fb968f618d886495d1f
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1107,9 +1107,9 @@ public class VolumeDialogImpl implements VolumeDialog,
         final int alpha = useActiveColoring
                 ? Color.alpha(tint.getDefaultColor())
                 : getAlphaAttr(android.R.attr.secondaryContentAlpha);
+        final ColorStateList progressTint = useActiveColoring ? null : tint;
         if (tint == row.cachedTint) return;
-        row.slider.setProgressTintList(tint);
-        row.slider.setProgressBackgroundTintList(tint);
+        row.slider.setProgressTintList(progressTint);
         row.slider.setAlpha(((float) alpha) / 255);
         row.icon.setImageTintList(tint);
         row.icon.setImageAlpha(alpha);
