f25cf823ae8e46cd751471fbdb17865194bbd7a5
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-01-21 12:48:51 +0000

    Revert "SystemUI: Fix an issue where jumping to black after screen turned on"
    This reverts commit 0885454bd198dc03139758f7e748de9c15786e1d.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4950,7 +4950,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         @Override
         public void startDozing() {
-            if (!mDozingRequested && !isScreenTurningOnOrOn()) {
+            if (!mDozingRequested) {
                 mDozingRequested = true;
                 DozeLog.traceDozing(mContext, mDozing);
                 updateDozing();
@@ -4998,7 +4998,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 DozeLog.traceDozing(mContext, mDozing);
                 mWakefulnessLifecycle.dispatchStartedWakingUp();
                 updateDozing();
-                updateScrimController();
             }
         }
 
