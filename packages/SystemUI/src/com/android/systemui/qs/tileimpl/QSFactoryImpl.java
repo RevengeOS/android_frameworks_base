93e29daaef3ef54c35403a2c6f0f1ea1a619c08f
Bladeyy <getsuga369@gmail.com>
2019-03-23 12:37:23 +0000

    Revert "Add partial/full screenshot QS tile"
    This reverts commit a6b661fe5dd2fa6489b8f18392474bb8805abbb9.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,7 +43,6 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
-import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -114,8 +113,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new ExpandedDesktopTile(mHost);
             case "gaming":
                 return new GamingModeTile(mHost);
-            case "screenshot":
-                return new ScreenshotTile(mHost);
         }
 
         // Intent tiles.
