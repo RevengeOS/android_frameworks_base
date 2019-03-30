11a0a61f77d8e76b826c8488dd5b240d43b2b8fe
Lucchetto <lucchetto.tie@live.com>
2019-03-30 15:59:13 +0100

    Revert "fw/base: Enable home button wake"
    This reverts commit ef4b98b5bb847bb3bcbbab708ff3720818649283.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6754,12 +6754,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             }
 
-            case KeyEvent.KEYCODE_HOME:
-                if (down && !interactive) {
-                    isWakeKey = true;
-                }
-                break;
-
             case KeyEvent.KEYCODE_ENDCALL: {
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
