9121df6ab719a6a7d9afd1d663c615b2ebf547a1
Lucchetto <lucchetto.tie@live.com>
2019-04-13 16:04:08 +0200

    Add Recorder tile

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.RecorderTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -116,6 +117,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new GamingModeTile(mHost);
 	    case "screenshot":
                 return new ScreenshotTile(mHost);
+	    case "recorder":
+                return new RecorderTile(mHost);
         }
 
         // Intent tiles.
