f5aa62534125136d2ddc8ce1fc7455c273076a0f
Lucchetto <lucchetto.tie@live.com>
2019-05-19 09:55:36 +0200

    Revert "Let InCallUI handle proximity sensor for incoming calls"
    This reverts commit 4b66aa070471a2061306ce6a98ec5085b6ad544e.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -73,7 +73,6 @@ import android.provider.Settings.SettingNotFoundException;
 import android.service.dreams.DreamManagerInternal;
 import android.service.vr.IVrManager;
 import android.service.vr.IVrStateCallbacks;
-import android.telephony.TelephonyManager;
 import android.util.KeyValueListParser;
 import android.util.PrintWriterPrinter;
 import android.util.Slog;
@@ -5179,13 +5178,7 @@ public final class PowerManagerService extends SystemService
             // There is already a message queued;
             return;
         }
-
-        final TelephonyManager tm =
-                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        final boolean hasIncomingCall = tm.getCallState() == TelephonyManager.CALL_STATE_RINGING;
-
-        if (mProximityWakeSupported && mProximityWakeEnabled
-                && mProximitySensor != null && !hasIncomingCall) {
+        if (mProximityWakeSupported && mProximityWakeEnabled && mProximitySensor != null) {
             final Message msg = mHandler.obtainMessage(MSG_WAKE_UP);
             msg.obj = r;
             mHandler.sendMessageDelayed(msg, mProximityTimeOut);
