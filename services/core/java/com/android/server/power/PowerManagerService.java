d2aeaff8e14f90cd2fa4b0d5121482e6b4d1a02f
Lucchetto <lucchetto.tie@live.com>
2019-05-19 09:54:00 +0200

    Revert "Log when proximity blocks wake"
    This reverts commit 547891fad888bcd3a940a22a2451ad013aa8da2d.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -5241,8 +5241,6 @@ public final class PowerManagerService extends SystemService
                     if (distance >= PROXIMITY_NEAR_THRESHOLD ||
                             distance >= mProximitySensor.getMaximumRange()) {
                         r.run();
-                    } else {
-                        Slog.w(TAG, "Not waking up. Proximity sensor is blocked.");
                     }
                 }
 
