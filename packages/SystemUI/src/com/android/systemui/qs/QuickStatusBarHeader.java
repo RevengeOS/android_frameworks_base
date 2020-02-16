c754c0c228929182306d1b55dcf199b70ece2057
Lucchetto <lucchetto.tie@live.com>
2020-02-16 20:05:30 +0100

    QuickStatusBarHeader: hide mobile signal icon
    Change-Id: Ic351ccdf7894d6848d79cc1efdc0c90980458b18
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -199,6 +199,8 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 com.android.internal.R.string.status_bar_camera));
         ignored.add(mContext.getResources().getString(
                 com.android.internal.R.string.status_bar_microphone));
+        ignored.add(mContext.getResources().getString(
+                com.android.internal.R.string.status_bar_mobile));
         if (mPermissionsHubEnabled) {
             ignored.add(mContext.getResources().getString(
                     com.android.internal.R.string.status_bar_location));
