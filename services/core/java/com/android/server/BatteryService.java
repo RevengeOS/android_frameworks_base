PixelExperience/frameworks_base

framework: Implement light for popup camera
Change-Id: I2ff7729ceb72303071b91f7e76a915baf33ea431

services/core/java/com/android/server/BatteryService.java
@@ -83,6 +83,8 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.android.internal.util.custom.popupcamera.PopUpCameraUtils;
+
 /**
  * <p>BatteryService monitors the charging status, and charge level of the device
  * battery.  When these values change this service broadcasts the new values
@@ -269,6 +271,7 @@ public final class BatteryService extends SystemService {
                 updateBatteryWarningLevelLocked();
             }
         } else if (phase == PHASE_BOOT_COMPLETED) {
+            PopUpCameraUtils.lockBatteryLed(mContext, false);
             SettingsObserver mObserver = new SettingsObserver(new Handler());
             mObserver.observe();
         }
@@ -288,6 +291,9 @@ public final class BatteryService extends SystemService {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BATTERY_LIGHT_ENABLED),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -299,7 +305,9 @@ public final class BatteryService extends SystemService {
         public void update() {
             ContentResolver resolver = mContext.getContentResolver();
             Resources res = mContext.getResources();
-            mBatteryLightEnabled = Settings.System.getInt(resolver,
+            boolean batteryLightTempBlocked = Settings.System.getInt(resolver,
+                    Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED, 0) == 1;
+            mBatteryLightEnabled = !batteryLightTempBlocked && Settings.System.getInt(resolver,
                     Settings.System.BATTERY_LIGHT_ENABLED, mContext.getResources().getBoolean(
                         com.android.internal.R.bool.config_intrusiveBatteryLed) ? 1 : 0) == 1;
             updateLed();

RevengeOS/android_frameworks_base

services/core/java/com/android/server/BatteryService.java
@@ -83,6 +83,8 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.android.internal.util.custom.popupcamera.PopUpCameraUtils;
+
 /**
  * <p>BatteryService monitors the charging status, and charge level of the device
  * battery.  When these values change this service broadcasts the new values
@@ -275,6 +277,7 @@ public final class BatteryService extends SystemService {
                 updateBatteryWarningLevelLocked();
             }
         } else if (phase == PHASE_BOOT_COMPLETED) {
+            PopUpCameraUtils.blockBatteryLed(mContext, false);
             SettingsObserver mObserver = new SettingsObserver(new Handler());
             mObserver.observe();
         }
@@ -319,6 +322,9 @@ public final class BatteryService extends SystemService {
                         Settings.System.BATTERY_LIGHT_REALLYFULL_COLOR),
                         false, this, UserHandle.USER_ALL);
             }
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -332,7 +338,9 @@ public final class BatteryService extends SystemService {
             Resources res = mContext.getResources();
 
             // Battery light enabled
-            mLightEnabled = Settings.System.getInt(resolver,
+            boolean batteryLightTempBlocked = Settings.System.getInt(resolver,
+                    Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED, 0) == 1;
+            mLightEnabled = !batteryLightTempBlocked && Settings.System.getInt(resolver,
                     Settings.System.BATTERY_LIGHT_ENABLED, 1) != 0;
             mAllowBatteryLightOnDnd = Settings.System.getInt(resolver,
                     Settings.System.BATTERY_LIGHT_ALLOW_ON_DND, 1) == 1;