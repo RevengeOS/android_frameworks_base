ab962e522f084b613215b9a6f0a91073c505ec90
Lucchetto <lucchetto.tie@live.com>
2020-09-18 21:09:21 +0200

    Revert "FODCircleView: Show while bouncer is requesting Pattern or PIN"
    This reverts commit 3ef21df1d171cf002154bad43c197df5f78317b8.
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -16,7 +16,6 @@
 
 package com.android.systemui.biometrics;
 
-import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -37,8 +36,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
-import com.android.internal.widget.LockPatternUtils;
-import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.systemui.Dependency;
@@ -80,8 +77,6 @@ public class FODCircleView extends ImageView {
 
     private final ImageView mPressedView;
 
-    private LockPatternUtils mLockPatternUtils;
-
     private Timer mBurnInProtectionTimer;
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =
@@ -116,14 +111,11 @@ public class FODCircleView extends ImageView {
         @Override
         public void onKeyguardBouncerChanged(boolean isBouncer) {
             mIsBouncer = isBouncer;
-            if (mUpdateMonitor.isFingerprintDetectionRunning()) {
-                if (isPinOrPattern(mUpdateMonitor.getCurrentUser()) || !isBouncer) {
-                    show();
-                } else {
-                    hide();
-                }
-            } else {
+
+            if (isBouncer) {
                 hide();
+            } else if (mUpdateMonitor.isFingerprintDetectionRunning()) {
+                show();
             }
         }
 
@@ -208,8 +200,6 @@ public class FODCircleView extends ImageView {
 
         updatePosition();
         hide();
-
-        mLockPatternUtils = new LockPatternUtils(mContext);
 
         mUpdateMonitor = Dependency.get(KeyguardUpdateMonitor.class);
         mUpdateMonitor.registerCallback(mMonitorCallback);
@@ -428,20 +418,6 @@ public class FODCircleView extends ImageView {
                 mWindowManager.removeView(mPressedView);
             }
         }
-    }
-
-    private boolean isPinOrPattern(int userId) {
-        int passwordQuality = mLockPatternUtils.getActivePasswordQuality(userId);
-        switch (passwordQuality) {
-            // PIN
-            case DevicePolicyManager.PASSWORD_QUALITY_NUMERIC:
-            case DevicePolicyManager.PASSWORD_QUALITY_NUMERIC_COMPLEX:
-            // Pattern
-            case DevicePolicyManager.PASSWORD_QUALITY_SOMETHING:
-                return true;
-        }
-
-        return false;
     }
 
     private class BurnInProtectionTask extends TimerTask {
