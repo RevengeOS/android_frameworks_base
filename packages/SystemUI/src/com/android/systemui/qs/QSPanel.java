b5793a2736c63b004b5a3764bb83892f8f89c618
Lucchetto <lucchetto.tie@live.com>
2020-09-22 09:13:38 +0200

    SystemUI: animate bottom brightness slider correctly when media player is visible
        * The media player is transitions using an hax when QS is expanding
        * causing the brightness slider to jump during the animation.
        * Let's workaround this hax by animating the slider with the difference
        * in height between expanded player and collapsed player
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -638,10 +638,14 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         }
     }
 
-    private boolean shouldUseHorizontalLayout() {
+    boolean shouldUseHorizontalLayout() {
         return mUsingMediaPlayer && mMediaHost.getVisible()
                 && getResources().getConfiguration().orientation
                 == Configuration.ORIENTATION_LANDSCAPE;
+    }
+
+    boolean isMediaHostVisible() {
+        return mMediaHost.getVisible();
     }
 
     protected void reAttachMediaHost() {
