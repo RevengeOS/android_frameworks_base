8f3c9a6059729fc201db54b98bb4f4885043b360
Ethan Halsall <ethan.halsall@gmail.com>
2020-09-24 00:22:43 +0100

    SystemUI: forward port Status bar icon tuner [1/2]
    Based on - https://github.com/RevengeOS/android_frameworks_base/commit/a8dc18723cf16cd904bc7f0af574f1a8ce18d36a
    
    In android R google have moved to dagger and javax for dependency injection, lets migrate our implementation from Q to new api. Also while we are at it hide the tuner icon from footer.
    
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -315,8 +315,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
 
     private void updateVisibilities() {
         mSettingsContainer.setVisibility(mQsDisabled ? View.GONE : View.VISIBLE);
-        mSettingsContainer.findViewById(R.id.tuner_icon).setVisibility(
-                TunerService.isTunerEnabled(mContext) ? View.VISIBLE : View.INVISIBLE);
+        mSettingsContainer.findViewById(R.id.tuner_icon).setVisibility(View.INVISIBLE);
         final boolean isDemo = UserManager.isDeviceInDemoMode(mContext);
         mMultiUserSwitch.setVisibility(showUserSwitcher() ? View.VISIBLE : View.INVISIBLE);
         mEditContainer.setVisibility(isDemo || !mExpanded ? View.INVISIBLE : View.VISIBLE);
