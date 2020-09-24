0bdfdd1c42c2478494f72859f43a9cb854ec24d7
Karthick C <karthickrko61@gmail.com>
2020-09-24 21:35:37 +0000

    BatteryService: Implement vooc charging support
    * Realme devices support non standard charger type, i.e vooc charger that has its own crazy charge speeds
      luckily vooc charger can be determined upon the value of one node, Let's use that and update user when
      vooc charger is coonected and vooc charging is in progresss, Also guard this via a overlay flag and set
      it to false by default to preserve the default aosp behaviour for non standard chargers if user uses them.
    
    [@dev-harsh1998]: Forward Port to Android R's fuelgauge implementation
    
    Change-Id: If9a616cbdeeaaf658d960d2f92b26e1f928d2fb2
    Co-authored-by: Harshit Jain <god@hyper-labs.tech>
    Signed-off-by: Harshit Jain <god@hyper-labs.tech>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1719,7 +1719,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, false);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -2597,6 +2597,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         // change in battery temperature
         if (old.temperature != current.temperature) {
+            return true;
+        }
+
+        // change in VOOC charging while plugged in
+        if (nowPluggedIn && current.voocChargeStatus != old.voocChargeStatus) {
             return true;
         }
 
