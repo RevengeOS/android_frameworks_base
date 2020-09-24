ae7f34fb01cc4fa80a7693587b726a57377f1605
Karthick C <karthickrko61@gmail.com>
2020-09-24 21:35:37 +0000

    BatteryService: Implement vooc charging support
    * Realme devices support non standard charger type, i.e vooc charger that has its own crazy charge speeds
      luckily vooc charger can be determined upon the value of one node, Let's use that and update user when
      vooc charger is coonected and vooc charging is in progresss, Also guard this via a overlay flag and set
      it to false by default to preserve the default aosp behaviour for non standard chargers if user uses them.
    
    [@dev-harsh1998]: Forward Port to Android R's fuelgauge implementation
    
    Change-Id: If9a616cbdeeaaf658d960d2f92b26e1f928d2fb2
    Co-authored-by: Harshit Jain <god@hyper-labs.tech>
    Signed-off-by: Harshit Jain <god@hyper-labs.tech>

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -27,6 +27,9 @@ import static android.os.BatteryManager.EXTRA_TEMPERATURE;
 import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static android.os.BatteryManager.EXTRA_STATUS;
 
+//dev-harsh1998 Port vooc charging status to Android R
+import static android.os.BatteryManager.EXTRA_VOOC_CHARGER;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
@@ -45,6 +48,9 @@ public class BatteryStatus {
     public static final int CHARGING_REGULAR = 1;
     public static final int CHARGING_FAST = 2;
 
+    // dev-harsh1998 port vooc charging to Android R fuel gauge
+    public static final int CHARGING_VOOC = 3;
+
     public final int status;
     public final int level;
     public final int plugged;
@@ -54,9 +60,12 @@ public class BatteryStatus {
     public final int maxChargingWattage;
     public final int temperature;
 
+    // dev-harsh1998 port vooc charging to Android R fuel gauge
+    public final boolean voocChargeStatus;
+
     public BatteryStatus(int status, int level, int plugged, int health,
             int maxChargingCurrent, int maxChargingVoltage,
-            int maxChargingWattage, int temperature) {
+            int maxChargingWattage, int temperature, boolean voocChargeStatus) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
@@ -65,6 +74,7 @@ public class BatteryStatus {
         this.maxChargingVoltage = maxChargingVoltage;
         this.maxChargingWattage = maxChargingWattage;
         this.temperature = temperature;
+        this.voocChargeStatus = voocChargeStatus;
     }
 
     public BatteryStatus(Intent batteryChangedIntent) {
@@ -73,6 +83,7 @@ public class BatteryStatus {
         level = batteryChangedIntent.getIntExtra(EXTRA_LEVEL, 0);
         health = batteryChangedIntent.getIntExtra(EXTRA_HEALTH, BATTERY_HEALTH_UNKNOWN);
         temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
+        voocChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_VOOC_CHARGER, false);
 
         final int maxChargingMicroAmp = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_CURRENT,
                 -1);
@@ -146,7 +157,8 @@ public class BatteryStatus {
                 R.integer.config_chargingSlowlyThreshold);
         final int fastThreshold = context.getResources().getInteger(
                 R.integer.config_chargingFastThreshold);
-        return maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
+        return voocChargeStatus ? CHARGING_VOOC :
+                maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                 maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                         maxChargingWattage > fastThreshold ? CHARGING_FAST :
                                 CHARGING_REGULAR;
