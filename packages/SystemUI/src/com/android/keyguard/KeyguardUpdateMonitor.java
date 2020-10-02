0f262ce8054b72c41f280f6d346c95ca0b0eabf9
Ethan Halsall <ethan.halsall@gmail.com>
2020-10-02 23:24:19 +0100

    Pocket: Do not disable fingerprint listener in pocket
    - Service is too slow
    
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1973,7 +1973,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 shouldListenForFingerprintAssistant() || (mKeyguardOccluded && mIsDreaming))
                 && !mSwitchingUser && !isFingerprintDisabled(getCurrentUser())
                 && (!mKeyguardGoingAway || !mDeviceInteractive) && mIsPrimaryUser
-                && allowedOnBouncer && !mIsDeviceInPocket;
+                && allowedOnBouncer;
         return shouldListen;
     }
 
